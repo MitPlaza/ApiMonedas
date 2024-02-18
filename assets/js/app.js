b3356f134f616bdd6b8054c9350c1b550bb15efe
MItPlaza <m.plazaweb@gmail.com>
2024-02-18 12:56:03 -0300

    se agregaron validaciones al input monto y al botón calcular

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,8 @@ const apiURL = "https://mindicador.cl/api";
 let cambioValor
 let valorDolar
 let valorEuro 
+let miMoneda = datoPesos.value
+let opcionTxt
 
 
 
@@ -68,143 +70,13 @@ const agregarSelect = () => {
    select.addEventListener('change', () => {
 
    
-
+            
             cambioValor = select.value
             opcionTxt = select.options[select.selectedIndex].textContent;
             valoresTexto.textContent = `Últimos valores del ${opcionTxt}`;
-
+           
             
-            // si el select cambia las moneds muestra la gráfica
-            if(opcionTxt === "USD"){
-
-                async function getValores() {
-                    const endpoint = "https://mindicador.cl/api/dolar/";
-                    const res = await fetch(endpoint);
-                    const valorDolar = await res.json();
-                    return valorDolar;
-                }
-                
-                function prepararConfiguracionParaLaGrafica(valorDolar) {
-                    // Tomar solo los primeros 10 datos
-                    const primerosDiezDatos = valorDolar.serie.slice(0, 10);
-                
-                    // Creamos las variables necesarias para el objeto de configuración
-                    const tipoDeGrafica = "line";
-                    const nombresDeLasMonedas = primerosDiezDatos.map((dolar) => {
-                        const fecha = dolar.fecha.split('T')[0]; // Tomar solo la parte de la fecha
-                        return fecha;
-                    });
-                    const titulo = "Valor Dolares";
-                    const colorDeLinea = "red";
-                    const valores = primerosDiezDatos.map((dolar) => {
-                        const valor = dolar.valor;
-                        return Number(valor);
-                    });
-                
-                    // Creamos el objeto de configuración usando las variables anteriores
-                    const config = {
-                        type: tipoDeGrafica,
-                        data: {
-                            labels: nombresDeLasMonedas,
-                            datasets: [
-                                {
-                                    label: titulo,
-                                    backgroundColor: colorDeLinea,
-                                    data: valores
-                                }
-                            ]
-                        }
-                    };
-                    return config;
-                }
-                
-                async function renderGrafica() {
-                    const valorDolar = await getValores();
-                    const config = prepararConfiguracionParaLaGrafica(valorDolar);
-                    const chartDOM = document.getElementById("myChart");
-                    chartDOM.style.backgroundColor = "#f2f2f2"
-                    
-                    // Destruir el gráfico existente si existe
-                    if (myChart) {
-                        myChart.destroy();
-                    }
-                    
-                    myChart = new Chart(chartDOM, config); // Crear el nuevo gráfico
-                }
-                renderGrafica();
-
-            }else if (opcionTxt === "EUR") {
-
-                async function getValores() {
-
-                    try{
-
-                    const endpoint = "https://mindicador.cl/api/euro/";
-                    const res = await fetch(endpoint);
-                    const valorEuro = await res.json();
-                    return valorEuro;
-
-                } catch (error){
-
-                    alert("error")
-
-                }
-
-                }
-                
-                function prepararConfiguracionParaLaGrafica(valorEuro) {
-                    // Tomar solo los primeros 10 datos
-                    const primerosDiezDatos = valorEuro.serie.slice(0, 10);
-                
-                    // Creamos las variables necesarias para el objeto de configuración
-                    const tipoDeGrafica = "line";
-                    const nombresDeLasMonedas = primerosDiezDatos.map((euro) => {
-                        const fecha = euro.fecha.split('T')[0]; // Tomar solo la parte de la fecha
-                        return fecha;
-                    });
-                    const titulo = "Valor Euros";
-                    const colorDeLinea = "red";
-                    const valores = primerosDiezDatos.map((euro) => {
-                        const valor = euro.valor;
-                        return Number(valor);
-                    });
-                
-                    // Creamos el objeto de configuración usando las variables anteriores
-                    const config = {
-                        type: tipoDeGrafica,
-                        data: {
-                            labels: nombresDeLasMonedas,
-                            datasets: [
-                                {
-                                    label: titulo,
-                                    backgroundColor: colorDeLinea,
-                                    data: valores
-                                }
-                            ]
-                        }
-                    };
-                    return config;
-                }
-                
-                async function renderGrafica() {
-                    const valorEuro = await getValores();
-                    const config = prepararConfiguracionParaLaGrafica(valorEuro);
-                    const chartDOM = document.getElementById("myChart");
-                    chartDOM.style.backgroundColor = "#f2f2f2"
-                    
-                    // Destruir el gráfico existente si existe
-                    if (myChart) {
-                        myChart.destroy();
-                    }
-                    
-                    myChart = new Chart(chartDOM, config); // Crear el nuevo gráfico
-                }
-                renderGrafica();
-                
-
-            } else{
-                console.log("falló")
-            }
+          
     })
 
     
@@ -215,26 +87,190 @@ agregarSelect()
 
 
 
+
+
+
+
+
+function calculaDolares(){
+
+    async function getValores() {
+        const endpoint = "https://mindicador.cl/api/dolar/";
+        const res = await fetch(endpoint);
+        const valorDolar = await res.json();
+        return valorDolar;
+    }
+    
+    function prepararConfiguracionParaLaGrafica(valorDolar) {
+        // Tomar solo los primeros 10 datos
+        const primerosDiezDatos = valorDolar.serie.slice(0, 10);
+    
+        // Creamos las variables necesarias para el objeto de configuración
+        const tipoDeGrafica = "line";
+        const nombresDeLasMonedas = primerosDiezDatos.map((dolar) => {
+            const fecha = dolar.fecha.split('T')[0]; // Tomar solo la parte de la fecha
+            return fecha;
+        });
+        const titulo = "Valor Dolares";
+        const colorDeLinea = "red";
+        const valores = primerosDiezDatos.map((dolar) => {
+            const valor = dolar.valor;
+            return Number(valor);
+        });
+    
+        // Creamos el objeto de configuración usando las variables anteriores
+        const config = {
+            type: tipoDeGrafica,
+            data: {
+                labels: nombresDeLasMonedas,
+                datasets: [
+                    {
+                        label: titulo,
+                        backgroundColor: colorDeLinea,
+                        data: valores
+                    }
+                ]
+            }
+        };
+        
+        return config;
+        
+    }
+    
+    async function renderGrafica() {
+        const valorDolar = await getValores();
+        const config = prepararConfiguracionParaLaGrafica(valorDolar);
+        const chartDOM = document.getElementById("myChart");
+        chartDOM.style.backgroundColor = "#f2f2f2"
+        
+        // Destruir el gráfico existente si existe
+        if (myChart) {
+            myChart.destroy();
+        }
+        
+        myChart = new Chart(chartDOM, config); // Crear el nuevo gráfico
+    }
+    renderGrafica();
+
+}
+
+function calculaEuros(){
+
+    async function getValores() {
+
+        try{
+
+        const endpoint = "https://mindicador.cl/api/euro/";
+        const res = await fetch(endpoint);
+        const valorEuro = await res.json();
+        return valorEuro;
+
+    } catch (error){
+
+        alert("error")
+
+    }
+
+    }
+    
+    function prepararConfiguracionParaLaGrafica(valorEuro) {
+        // Tomar solo los primeros 10 datos
+        const primerosDiezDatos = valorEuro.serie.slice(0, 10);
+    
+        // Creamos las variables necesarias para el objeto de configuración
+        const tipoDeGrafica = "line";
+        const nombresDeLasMonedas = primerosDiezDatos.map((euro) => {
+            const fecha = euro.fecha.split('T')[0]; // Tomar solo la parte de la fecha
+            return fecha;
+        });
+        const titulo = "Valor Euros";
+        const colorDeLinea = "red";
+        const valores = primerosDiezDatos.map((euro) => {
+            const valor = euro.valor;
+            return Number(valor);
+        });
+    
+        // Creamos el objeto de configuración usando las variables anteriores
+        const config = {
+            type: tipoDeGrafica,
+            data: {
+                labels: nombresDeLasMonedas,
+                datasets: [
+                    {
+                        label: titulo,
+                        backgroundColor: colorDeLinea,
+                        data: valores
+                    }
+                ]
+            }
+        };
+        return config;
+    }
+    
+    async function renderGrafica() {
+        const valorEuro = await getValores();
+        const config = prepararConfiguracionParaLaGrafica(valorEuro);
+        const chartDOM = document.getElementById("myChart");
+        chartDOM.style.backgroundColor = "#f2f2f2"
+        
+        // Destruir el gráfico existente si existe
+        if (myChart) {
+            myChart.destroy();
+        }
+        
+        myChart = new Chart(chartDOM, config); // Crear el nuevo gráfico
+    }
+    renderGrafica();
+
+
+}
+
+
+
 //calcular valor de la conversión
 
-btnCalcular.addEventListener('click', () => {
-
-    let miMoneda = datoPesos.value
-    miMoneda = parseFloat(miMoneda)
-
-    let calculoTotal = miMoneda / cambioValor
-    valorTotal.textContent = "Resultado: " + calculoTotal.toFixed(2) + " " + opcionTxt;
-
-
-
-})
-  
-
-
-
-
-
+function activarBtn(){
+      
+    datoPesos.addEventListener('click', () => {
     
+    
+        btnCalcular.disabled = false;
+    
+        btnCalcular.addEventListener('click', () => {
+        
+        
+        
+            let miMoneda = datoPesos.value
+            miMoneda = parseFloat(miMoneda)
+        
+            let calculoTotal = miMoneda / cambioValor
+            valorTotal.textContent = "Resultado: " + calculoTotal.toFixed(2) + " " + opcionTxt;
+            
+              // si el select cambia las moneds muestra la gráfica
+    if(opcionTxt === "USD"){
+    
+        //funcion para activar el valor
+        calculaDolares()
+
+     }else if (opcionTxt === "EUR") {
+
+         calculaEuros()
+         
+
+     } else{
+         console.log("falló")
+     }
+           
+        })
+    
+      
+    
+    })
+      
+    
+    
+    }
+    activarBtn()
     
   
 
